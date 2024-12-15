2e88c97544d0e1d1e6ec0faaa5167e675dc81a09
oleksandr <aleksandr.savelyev@gmail.com>
2024-12-15 16:27:32 +0200

    add ExceptionsListener

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,11 @@
 
 namespace App\Controller;
 
-use App\Exception\LimitExceededException;
 use App\Service\TransactionService;
 use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -25,31 +23,17 @@ class MainController extends AbstractController
     #[Route('/limits/{userId}', name: 'get_limits', methods: ['GET'], requirements: ['userId' => '\d+'])]
     public function getLimits(int $userId): JsonResponse
     {
-        try {
-            $user = $this->userService->getUserById($userId);
-            $limits = $this->userService->getUserLimits($user);
+        $user = $this->userService->getUserById($userId);
+        $limits = $this->userService->getUserLimits($user);
 
-            return $this->json($limits, JsonResponse::HTTP_OK);
-        } catch (NotFoundHttpException $e) {
-            return $this->json([
-                'error' => $e->getMessage(),
-            ], JsonResponse::HTTP_NOT_FOUND);
-        } catch (\Throwable $e) {
-            return $this->json([
-                'error' => $e->getMessage(),
-            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return $this->json($limits, JsonResponse::HTTP_OK);
     }
 
     #[Route('/transactions', name: 'create_transaction', methods: ['POST'])]
     public function createTransaction(Request $request): JsonResponse
     {
-        try {
-            $data = $this->transactionService->parseJson($request);
+        $data = $this->transactionService->parseJson($request);
 
-            return $this->transactionService->handleTransactionCreation($data);
-        } catch (\Throwable $e) {
-            return $this->json(['error' => $e->getMessage()], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return $this->transactionService->handleTransactionCreation($data);
     }
 }
---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Psr\Log\LoggerInterface;
+
+class ExceptionListener
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        // TODO: добавить корректную обработку эксепшенов
+        $exception = $event->getThrowable();
+
+        // TODO: добавтиь логирование
+        // $this->logger->error($exception->getMessage(), ['exception' => $exception]);
+
+        $statusCode = JsonResponse::HTTP_INTERNAL_SERVER_ERROR;
+        $message = 'An unexpected error occurred';
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $statusCode = $exception->getStatusCode();
+            $message = $exception->getMessage();
+        }
+
+        $response = new JsonResponse(
+            ['error' => $message],
+            $statusCode
+        );
+
+        $event->setResponse($response);
+    }
+}
